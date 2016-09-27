2d84d3672dc10b445492cc2d5278141ecd133e5d
Christian Sandberg <christiansandberg@me.com>
2016-09-27 10:00:11 +0200

    Add some more SDO abort codes.

--- canopen/sdo.py
+++ canopen/sdo.py
@@ -248,13 +248,21 @@ class SdoAbortedError(SdoError):
     """SDO abort exception."""
 
     CODES = {
+        0x05030000: "SDO toggle bit error",
         0x05040000: "Timeout of transfer communication detected",
+        0x05040001: "Unknown SDO command specified",
+        0x05040003: "Invalid sequence number",
         0x06010000: "Unsupported access to an object",
         0x06010001: "Attempt to read a write only object",
         0x06010002: "Attempt to write a read only object",
+        0x06020000:	"Object does not exist",
+        0x06040042:	"PDO length exceeded",
         0x06060000: "Access failed due to a hardware error",
+        0x06070010: "Data type and length code do not match",
+        0x06090011:	"Subindex does not exist",
         0x06090030: "Value range of parameter exceeded",
         0x060A0023: "Resource not available",
+        0x08000000: "General error",
         0x08000021: ("Data can not be transferred or stored to the application "
                      "because of local control"),
         0x08000022: ("Data can not be transferred or stored to the application "
