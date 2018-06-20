a11372f560cc76fd9098502b3e06cd99cdacb55f
André Colomb <src@andre.colomb.de>
2018-06-20 13:53:16 +0200

    Add a configurable pause when sending SDOs.
    Some slave controllers cannot handle messages coming in too fast.
    When mapping a PDO for example, python-canopen sends multiple SDO
    queries in direct succession.
    
    This adds an attribute SdoClient.pause_before_send, which can be set
    on a node's sdo object.  If non-zero, time.sleep() is inserted before
    each send_message() call in send_request().

--- canopen/sdo.py
+++ canopen/sdo.py
@@ -74,6 +74,7 @@ class SdoClient(collections.Mapping):
         self.tx_cobid = tx_cobid
         self.network = None
         self.od = od
+        self.pause_before_send = 0.0
         self.responses = queue.Queue()
 
     def on_response(self, can_id, data, timestamp):
@@ -83,6 +84,8 @@ class SdoClient(collections.Mapping):
         retries_left = self.MAX_RETRIES
         while True:
             try:
+                if self.pause_before_send:
+                    time.sleep(self.pause_before_send)
                 self.network.send_message(self.rx_cobid, request)
             except CanError as e:
                 # Could be a buffer overflow. Wait some time before trying again
