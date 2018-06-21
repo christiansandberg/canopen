da94e6de684af331d27353d7b22cbaf8a76790f9
André Colomb <src@andre.colomb.de>
2018-06-21 12:50:34 +0200

    Use pdo.Variable.subindex property instead of deferring to od.
    In case the variable's subindex was overridden, that value is now used
    during pdo.Map.save() and in log messages.

--- canopen/pdo.py
+++ canopen/pdo.py
@@ -311,9 +311,9 @@ class Map(object):
             subindex = 1
             for var in self.map:
                 logger.info("Writing %s (0x%X:%d, %d bits) to PDO map",
-                            var.name, var.od.index, var.od.subindex, var.length)
+                            var.name, var.od.index, var.subindex, var.length)
                 self.map_array[subindex].raw = (var.od.index << 16 |
-                                                var.od.subindex << 8 |
+                                                var.subindex << 8 |
                                                 var.length)
                 subindex += 1
             self.map_array[0].raw = len(self.map)
@@ -346,7 +346,7 @@ class Map(object):
             # Custom bit length
             var.length = length
         logger.info("Adding %s (0x%X:%d, %d bits) to PDO map",
-                    var.name, var.od.index, var.od.subindex, var.length)
+                    var.name, var.od.index, var.subindex, var.length)
         self.map.append(var)
         self.length += var.length
         self._update_data_size()
