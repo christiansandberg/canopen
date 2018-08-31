fc212f6aa1a9d71aa0d96e4de7cc439d4d120e36
André Filipe Silva <andre.silva@introsys.eu>
2018-08-31 11:39:43 +0100

    updated the test unit

--- test/test_pdo.py
+++ test/test_pdo.py
@@ -30,6 +30,11 @@ class TestPDO(unittest.TestCase):
         self.assertEqual(map['INTEGER8 value'].raw, -2)
         self.assertEqual(map['INTEGER32 value'].raw, 0x01020304)
 
+        self.assertEqual(node.tpdo[1]['INTEGER16 value'].raw, -3)
+        self.assertEqual(node.tpdo[1]['UNSIGNED8 value'].raw, 0xf)
+        self.assertEqual(node.tpdo[1]['INTEGER8 value'].raw, -2)
+        self.assertEqual(node.tpdo[1]['INTEGER32 value'].raw, 0x01020304)
+
 
 if __name__ == "__main__":
     unittest.main()
