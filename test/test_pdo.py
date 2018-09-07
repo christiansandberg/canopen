5d92e8d8792ff34b959fcc3583cde94533edc5ad
André Filipe Silva <andre.silva@introsys.eu>
2018-09-07 12:11:03 +0100

    update access to PDO objects and added some tests to validate
    Now can be access by the PDO global list (if more that one PDO contains a specific object the first one is retrieved).
    Before one would need to specify the specific PDO and then the object mapped, now we can search in the all list.

--- test/test_pdo.py
+++ test/test_pdo.py
@@ -10,10 +10,10 @@ class TestPDO(unittest.TestCase):
     def test_bit_mapping(self):
         node = canopen.Node(1, EDS_PATH)
         map = node.pdo.tx[1]
-        map.add_variable('INTEGER16 value')
-        map.add_variable('UNSIGNED8 value', length=4)
-        map.add_variable('INTEGER8 value', length=4)
-        map.add_variable('INTEGER32 value')
+        map.add_variable('INTEGER16 value')           # 0x2001
+        map.add_variable('UNSIGNED8 value', length=4) # 0x2002
+        map.add_variable('INTEGER8 value', length=4)  # 0x2003
+        map.add_variable('INTEGER32 value')           # 0x2004
 
         # Write some values
         map['INTEGER16 value'].raw = -3
@@ -42,6 +42,12 @@ class TestPDO(unittest.TestCase):
         self.assertEqual(node.pdo[0x1600]['INTEGER16 value'].raw, -3)
         self.assertEqual(node.pdo['INTEGER16 value'].raw, -3)
         self.assertEqual(node.pdo.tx[1]['INTEGER16 value'].raw, -3)
+        self.assertEqual(node.pdo[0x2001].raw, -3)
+        self.assertEqual(node.tpdo[0x2001].raw, -3)
+        self.assertEqual(node.pdo[0x2002].raw, 0xf)
+        self.assertEqual(node.tpdo[0x2002].raw, 0xf)
+        self.assertEqual(node.pdo[0x1600][0x2002].raw, 0xf)
+    
         
 
 
