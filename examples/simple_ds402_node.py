f1474f73fadd5dd8427916d0b740dd616e55204f
André Filipe Silva <andre.silva@introsys.eu>
2018-09-11 19:11:27 +0100

    updated example

--- examples/simple_ds402_node.py
+++ examples/simple_ds402_node.py
@@ -11,7 +11,12 @@ try:
     network = canopen.Network()
 
     # Connect to the CAN bus
-    network.connect(bustype='kvaser', channel=0, bitrate=1000000)
+    
+    kargs = {'bustype': 'kvaser', 'channel':0, 'bitrate': 1000000}
+    network.connect(**kargs)
+
+    
+    #network.connect(bustype='kvaser', channel=0, bitrate=1000000)
 
     network.check()
 
@@ -29,31 +34,35 @@ try:
     print 'node state 1) = {0}'.format(node.nmt.state)
 
     # Iterate over arrays or records
-    error_log = node.sdo[0x1003]
-    for error in error_log.values():
-        print "Error {0} was found in the log".format(error.raw)
-
-    for node_id in network:
-        print network[node_id]
-
-    print 'node state 2) = {0}'.format(node.nmt.state)
+#    error_log = node.sdo[0x1003]
+#    for error in error_log.values():
+#        print "Error {0} was found in the log".format(error.raw)
+#
+#    for node_id in network:
+#        print network[node_id]
+#
+#    print 'node state 2) = {0}'.format(node.nmt.state)
 
     # Read a variable using SDO
 
-    node.sdo[0x1006].raw = 1
-    node.sdo[0x100c].raw = 100
-    node.sdo[0x100d].raw = 3
-    node.sdo[0x1014].raw = 163
-    node.sdo[0x1003][0].raw = 0
+#    node.sdo[0x1006].raw = 1
+#    node.sdo[0x100c].raw = 100
+#    node.sdo[0x100d].raw = 3
+#    node.sdo[0x1014].raw = 163
+#    node.sdo[0x1003][0].raw = 0
 
     # Transmit SYNC every 100 ms
     network.sync.start(0.1)
+    node.setup_402_state_machine()
+
+    node.state = 'READY TO SWITCH ON'
+    node.state = 'SWITCHED ON'
 
     node.load_configuration()
 
     print 'node state 3) = {0}'.format(node.state)
 
-    node.setup_402_state_machine()
+    
     node.op_mode = 'PROFILED POSITION'
 
     device_name = node.sdo[0x1008].raw
@@ -64,25 +73,25 @@ try:
 
     print 'node state 4) = {0}'.format(node.nmt.state)
 
-    # Read PDO configuration from node
-    node.tpdo.read()
-    # Re-map TxPDO1
-    node.tpdo[1].clear()
-    node.tpdo[1].add_variable('Statusword')
-    node.tpdo[1].add_variable('Velocity actual value')
-    node.tpdo[1].trans_type = 1
-    node.tpdo[1].event_timer = 0
-    node.tpdo[1].enabled = True
-    # Save new PDO configuration to node
-    node.tpdo.save()
-
-    # publish the a value to the control word (in this case reset the fault at the motors)
-
-    node.rpdo.read()
-    node.rpdo[1]['Controlword'].raw = 0x80
-    node.rpdo[1].transmit()
-    node.rpdo[1][0x6040].raw = 0x81
-    node.rpdo[1].transmit()
+#    # Read PDO configuration from node
+#    node.tpdo.read()
+#    # Re-map TxPDO1
+#    node.tpdo[1].clear()
+#    node.tpdo[1].add_variable('Statusword')
+#    node.tpdo[1].add_variable('Velocity actual value')
+#    node.tpdo[1].trans_type = 1
+#    node.tpdo[1].event_timer = 0
+#    node.tpdo[1].enabled = True
+#    # Save new PDO configuration to node
+#    node.tpdo.save()
+#
+#    # publish the a value to the control word (in this case reset the fault at the motors)
+#
+#    node.rpdo.read()
+#    node.rpdo[1]['Controlword'].raw = 0x80
+#    node.rpdo[1].transmit()
+#    node.rpdo[1][0x6040].raw = 0x81
+#    node.rpdo[1].transmit()
 
     node.rpdo.export('database.dbc')
 
