b1f1d23eab9c054734146082ee5d92283f0f103b
André Filipe Silva <andre.silva@introsys.eu>
2018-09-19 14:33:26 +0100

    removed @classmethod

--- canopen/profiles/p402.py
+++ canopen/profiles/p402.py
@@ -391,7 +391,6 @@ class BaseNode402(RemoteNode):
         except KeyError:
             raise KeyError('The object 0x6041 (Statusword) is not configured in this device.')
 
-    @classmethod
     @statusword.setter
     def statusword(self, value):
         raise RuntimeError('This property has no setter. The value {0} will be discarded'.format(value))
