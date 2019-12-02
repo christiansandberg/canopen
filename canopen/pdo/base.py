f5c6ae1ef76c7299fe91efa3b1a007ba3a0d029e
Sander Huijsen <sander.huijsen@gmail.com>
2019-12-02 14:35:48 +0800

    In stead of assigning 0 directly to a timestamp member, make it None, then check whether it exists before doing the subtraction. This way, we can potentially use other types for the timestamp, as long as subtracting two of them result in an integer/float (meaning seconds).

--- canopen/pdo/base.py
+++ canopen/pdo/base.py
@@ -174,7 +174,7 @@ class Map(object):
         #: Current message data
         self.data = bytearray()
         #: Timestamp of last received message
-        self.timestamp = 0
+        self.timestamp = None
         #: Period of receive message transmission in seconds
         self.period = None
         self.callbacks = []
@@ -268,7 +268,7 @@ class Map(object):
             with self.receive_condition:
                 self.is_received = True
                 self.data = data
-                self.period = timestamp - self.timestamp
+                self.period = timestamp - self.timestamp if self.timestamp is not None else timestamp
                 self.timestamp = timestamp
                 self.receive_condition.notify_all()
                 for callback in self.callbacks:
