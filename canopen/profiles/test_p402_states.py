f361b6b85ffc5ee196544c2bde26053884574f6e
André Colomb <src@andre.colomb.de>
2021-08-13 00:25:02 +0200

    p402: Extend test script to check the actual implementation.
    Mock up a BaseNode402 object and compare the _next_state() behavior to
    the simple lookups from the underlying transition tables.

--- canopen/profiles/test_p402_states.py
+++ canopen/profiles/test_p402_states.py
@@ -1,7 +1,10 @@
-from canopen.profiles.p402 import State402
+from canopen.objectdictionary import ObjectDictionary
+from canopen.profiles.p402 import State402, BaseNode402
 
 
 if __name__ == '__main__':
+    n = BaseNode402(1, ObjectDictionary())
+
     for target_state in State402.SW_MASK:
         print('\n--- Target =', target_state, '---')
         for from_state in State402.SW_MASK:
@@ -15,3 +18,12 @@ if __name__ == '__main__':
                     print('FAIL:\t{} -> {}'.format(from_state, next_state))
                 else:
                     print('\t{} -> {} ...'.format(from_state, next_state))
+
+            try:
+                while from_state != target_state:
+                    n.tpdo_values[0x6041] = State402.SW_MASK[from_state][1]
+                    next_state = n._next_state(target_state)
+                    print('\t\t-> {}'.format(next_state))
+                    from_state = next_state
+            except ValueError:
+                print('\t\t-> disallowed!')
