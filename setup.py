c46228f9cf2d2661166d68e7175f3e8b99064194
Christian Sandberg <christiansandberg@me.com>
2021-10-10 20:12:30 +0200

    Use PEP 517 for packaging

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-description = open("README.rst").read()
-# Change links to stable documentation
-description = description.replace("/latest/", "/stable/")
-
-setup(
-    name="canopen",
-    url="https://github.com/christiansandberg/canopen",
-    use_scm_version=True,
-    packages=find_packages(),
-    author="Christian Sandberg",
-    author_email="christiansandberg@me.com",
-    description="CANopen stack implementation",
-    keywords="CAN CANopen",
-    long_description=description,
-    license="MIT",
-    platforms=["any"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering"
-    ],
-    install_requires=["python-can>=3.0.0"],
-    extras_require={
-        "db_export": ["canmatrix"]
-    },
-    setup_requires=["setuptools_scm"],
-    include_package_data=True
-)
+setup()
