61ff2233c4837231a767e53ba6c1e86671aec1fc
Stefan Wolf <s.wolf@pinion.eu>
2022-10-06 16:22:50 +0200

    change setup py to work properly with vscode

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    readme_content = f.read()
+
+# This call to setup() does all the work
+setup(
+    name='canopen',
+    packages=find_packages(),
+    version='2.1.0',
+    description='CANopen stack implementation',
+    long_description=readme_content,
+    long_description_content_type='text/markdown',
+    author='Christian Sandberg',
+    license='MIT',
+    install_requires=['can'],
+)
